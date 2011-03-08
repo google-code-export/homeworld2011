9659cc67a4ed7bea16030b3e4b6aca9cb016cca6
krytra@gmail.com <krytra@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-08 23:16:54 +0000

    Nowa solucja projektu, podstawa do dalszego rozwoju. Prawie całkowicie pusta głowna klasa 'Game'. Jako bonus prosta statyczna klasa do diagnostyki z usprawnionym loggerem znanym z poprzedniego projektu.

---
+++ Zaraza/trunk/Plague/Plague/Diagnostics.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+/************************************************************************************/
+/// Plague
+/************************************************************************************/
+namespace Plague
+{
+
+    /********************************************************************************/
+    /// Diagnostics
+    /********************************************************************************/
+    static class Diagnostics
+    {
+
+        /****************************************************************************/
+        /// Fields
+        /****************************************************************************/
+        private static long         fPS                 = 0;
+        private static long         frames              = 0;
+        private static TimeSpan     elapsedTime         = TimeSpan.Zero;
+        private static TimeSpan     totalElapsedTime    = TimeSpan.Zero;
+        
+        private static bool         forceGCOnUpdate     = false;
+        
+        private static Game         game                = null;
+        private static bool         showDiagnostics     = true;
+        private static TextWriter   textWriter          = null;
+        private static String       logFile             = String.Empty;
+        private static LogWindow    logWindow           = null;
+        private static bool         showLogWindow       = false;
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Update
+        /****************************************************************************/
+        public static void Update(TimeSpan deltaTime)
+        {
+            ++frames;
+
+            elapsedTime += deltaTime;
+            totalElapsedTime += deltaTime;
+
+            if (elapsedTime.Seconds >= 1)
+            {
+                fPS = frames / elapsedTime.Seconds;
+                frames = 0;
+                elapsedTime = TimeSpan.Zero;
+            }
+
+            if (forceGCOnUpdate) GC.Collect();
+            if (showDiagnostics) game.Window.Title = game.Title + " | " + Diagnostics.ToString();
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Open Log File
+        /****************************************************************************/
+        public static bool OpenLogFile(String directory = null)
+        {
+            if (textWriter == null)
+            {
+                logFile = (directory == null ? String.Empty : directory + "\\") + DateTime.Now.ToString(@"HH-mm-ss dd-MM-yy") + ".txt";
+
+                try
+                {
+                    textWriter = new StreamWriter(logFile);
+                }
+                catch (IOException)
+                {
+                    if (directory != null)
+                    {
+                        Directory.CreateDirectory(directory + "\\");
+                        textWriter = new StreamWriter(logFile);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                
+                textWriter.WriteLine(game.Title);
+                textWriter.WriteLine(DateTime.Now.ToString());
+                textWriter.WriteLine("-------------------------");
+                textWriter.Flush();
+
+                if (logWindow != null)
+                {
+                    logWindow.TextBox.Text += game.Title                  + System.Environment.NewLine;
+                    logWindow.TextBox.Text += DateTime.Now.ToString()     + System.Environment.NewLine;
+                    logWindow.TextBox.Text += "-------------------------" + System.Environment.NewLine;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Push Log
+        /****************************************************************************/
+        public static void PushLog(String text)
+        {
+            if (textWriter == null) return;
+
+            textWriter.WriteLine(">> " + DateTime.Now.ToString(@"HH\:mm\:ss")     + " | "
+                                       + totalElapsedTime.ToString(@"hh\:mm\:ss") + " >> "
+                                       + text);
+            textWriter.Flush();
+
+            if (logWindow != null)
+            {
+                logWindow.TextBox.Text += ">> " + DateTime.Now.ToString(@"HH\:mm\:ss") + " | "
+                                       + totalElapsedTime.ToString(@"hh\:mm\:ss")      + " >> "
+                                       + text + System.Environment.NewLine;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Close Log File
+        /****************************************************************************/
+        public static void CloseLogFile()
+        {
+            if (textWriter != null)
+            {
+                textWriter.WriteLine("-------------------------");
+                textWriter.WriteLine("Run Time: " + totalElapsedTime.ToString(@"hh\:mm\:ss"));
+                textWriter.Flush();
+                textWriter.Close();
+
+                if (logWindow != null)
+                {
+                    logWindow.Close();
+                    logWindow       = null;
+                    showLogWindow   = false;
+                }
+            }
+        }
+        /****************************************************************************/
+                     
+
+        /****************************************************************************/
+        /// FPS
+        /****************************************************************************/
+        public static long FPS
+        {
+            get
+            {
+                return fPS;                        
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Run Time
+        /****************************************************************************/
+        public static TimeSpan RunTime 
+        {
+            get
+            {
+                return totalElapsedTime;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Allocated Managed Memory
+        /****************************************************************************/
+        public static long AllocatedManagedMemory
+        {
+            get
+            {
+                return GC.GetTotalMemory(false);
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Force GC On Update
+        /****************************************************************************/
+        public static bool ForceGCOnUpdate
+        {
+            set
+            {
+                forceGCOnUpdate = value;
+            }
+
+            get
+            {
+                return forceGCOnUpdate;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Game
+        /****************************************************************************/
+        public static Game Game
+        {
+            set 
+            {
+                game = value;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// V Sync
+        /****************************************************************************/
+        public static bool VSync
+        {
+            set
+            {
+                game.IsFixedTimeStep = value;
+            }
+
+            get
+            {
+                return game.IsFixedTimeStep;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Show Diagnostics
+        /****************************************************************************/
+        public static bool ShowDiagnostics
+        {
+            set
+            {
+                showDiagnostics = value;
+                if (!showDiagnostics) game.Window.Title = game.Title;
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Show Log Window
+        /****************************************************************************/
+        public static bool ShowLogWindow
+        {
+            set
+            {
+                showLogWindow = value;
+                
+                if (showLogWindow && logWindow == null && textWriter != null)
+                {
+                    logWindow = new LogWindow();
+                    logWindow.Show();
+                    textWriter.Close();
+
+                    using (TextReader textReader = new StreamReader(logFile))
+                    {
+                        logWindow.TextBox.Text += textReader.ReadToEnd();
+                    }
+
+                    textWriter = new StreamWriter(logFile,true);
+                }
+                else if (showLogWindow && logWindow == null && textWriter == null)
+                {
+                    logWindow = new LogWindow();
+                    logWindow.Show();                
+                }
+                else if (!showLogWindow && logWindow != null)
+                {
+                    logWindow.Close();
+                    logWindow = null;
+                }
+            }
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// To String
+        /****************************************************************************/
+        public static String ToString()
+        {
+            return "FPS: "                          + fPS.ToString()                             + 
+                   " | Run Time: "                  + totalElapsedTime.ToString(@"hh\:mm\:ss")   +
+                   " | Allocated Managed Memory: "  + (GC.GetTotalMemory(false)/1024).ToString() + " KB";
+        }
+        /****************************************************************************/
+
+    }
+    /********************************************************************************/
+
+}
+/************************************************************************************/
---
+++ Zaraza/trunk/Plague/Plague/Game.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+
+/************************************************************************************/
+/// Plague
+/************************************************************************************/
+namespace Plague
+{
+    /********************************************************************************/
+    /// Game
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    /********************************************************************************/
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+
+        /****************************************************************************/
+        /// Fields
+        /****************************************************************************/
+        private String title = String.Empty;
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Constructor
+        /****************************************************************************/
+        public Game(String title)
+        {
+            this.title   = title;
+            Window.Title = title;      
+
+            Diagnostics.Game            = this;
+            Diagnostics.ShowDiagnostics = true;
+            Diagnostics.ForceGCOnUpdate = true;
+            Diagnostics.VSync           = false;
+            Diagnostics.ShowLogWindow   = true;            
+            Diagnostics.OpenLogFile("log");            
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Initialize
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        /****************************************************************************/
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+            Diagnostics.PushLog("Initialization complete");
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Load Content
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        /****************************************************************************/
+        protected override void LoadContent()
+        {
+            // TODO: use this.Content to load your game content here
+            Diagnostics.PushLog("Loading content complete");
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Unload Content
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        /****************************************************************************/
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            Diagnostics.PushLog("Unloading content complete");
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Update
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /****************************************************************************/
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            Diagnostics.Update(gameTime.ElapsedGameTime);
+            base.Update(gameTime);
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Draw
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /****************************************************************************/
+        protected override void Draw(GameTime gameTime)
+        {
+            // TODO: Add your drawing code here            
+            base.Draw(gameTime);           
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// On Exiting
+        /****************************************************************************/
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            base.OnExiting(sender, args);
+            Diagnostics.PushLog("Exiting");
+            Diagnostics.CloseLogFile();
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Title
+        /****************************************************************************/
+        public String Title
+        {
+            set
+            {
+                title = value;
+            }
+
+            get
+            {
+                return title;
+            }
+        }
+        /****************************************************************************/
+
+    }
+    /********************************************************************************/
+
+}
+/************************************************************************************/
