5374d52556515af70f1c84ef50a667d366fcaff6
krytra@gmail.com <krytra@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-08 23:16:54 +0000

    Nowa solucja projektu, podstawa do dalszego rozwoju. Prawie całkowicie pusta głowna klasa 'Game'. Jako bonus prosta statyczna klasa do diagnostyki z usprawnionym loggerem znanym z poprzedniego projektu.

---
+++ Zaraza/trunk/Plague/Plague/LogWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Plague
+{
+    public partial class LogWindow : Form
+    {
+        public TextBox TextBox
+        {
+            get
+            {
+                return this.textBox;
+            }
+        }
+        
+        public LogWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
