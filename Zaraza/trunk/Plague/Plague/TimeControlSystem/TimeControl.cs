854bdce9394fbfe17bd0a0a0314c2718893eab44
krytra@gmail.com <krytra@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-09 18:42:13 +0000

    Poprawka do textboxa z logiem (teraz poprawnie scrolluje się w dół). Dodany nowy system kontroli czasu z zegarami i timerami.

---
+++ Zaraza/trunk/Plague/Plague/TimeControlSystem/TimeControl.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/************************************************************************************/
+/// PlagueEngine.TimeControlSystem
+/************************************************************************************/
+namespace PlagueEngine.TimeControlSystem
+{
+  
+    /********************************************************************************/
+    /// Time Control    
+    /// <summary>
+    /// Statyczna klasa do zarządzania zegarami i timerami. Pozwala na tworzenie ich 
+    /// w kontekście czasu działania aplikacji. 
+    /// </summary>
+    /********************************************************************************/
+    static class TimeControl
+    {
+
+        /****************************************************************************/
+        /// Fields
+        /****************************************************************************/
+        private static Dictionary<uint,Timer> Timers       = new Dictionary<uint,Timer>();
+        private static List<Timer>            WastedTimers = new List<Timer>();
+        private static List<Clock>            Clocks       = new List<Clock>();
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Update
+        /****************************************************************************/
+        public static void Update(TimeSpan deltaTime)
+        {
+            foreach (Timer timer in Timers.Values)
+            {
+                if (timer.Wasted) WastedTimers.Add(timer);               
+                else timer.Update(deltaTime);
+            }
+
+            foreach (Clock clock in Clocks)
+            {
+                clock.Update(deltaTime);
+            }
+
+            foreach (Timer timer in WastedTimers) Timers.Remove(timer.ID);
+            WastedTimers.Clear();
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Create Timer
+        /// <summary>
+        /// Tworzy nowy timer.
+        /// </summary>
+        /// <param name="alarm">Czas w sekundach, po ilu zostanie uruchomiony alarm.</param>
+        /// <param name="repeat">Powtarzalność alarmu. -1 : nieskończoność, 0 : brak, > 0 : krotność.</param>
+        /// <param name="callback">Metoda która zostanie wywołana jako alarm</param>
+        /// <returns>ID Timera.</returns>         
+        /****************************************************************************/
+        public static uint CreateTimer(TimeSpan alarm, int repeat, Timer.CallbackDelegate callback)
+        {
+            Timer timer = new Timer(alarm, repeat, callback);
+            Timers.Add(timer.ID, timer);
+            return timer.ID;            
+        }
+        /****************************************************************************/
+
+        
+        /****************************************************************************/
+        /// Create Timer (2)
+        /// <summary>
+        /// Tworzy nowy timer.
+        /// </summary>
+        /// <param name="alarm">Czas w sekundach, po ilu zostanie uruchomiony alarm.</param>
+        /// <param name="repeat">Powtarzalność alarmu. -1 : nieskończoność, 0 : brak, > 0 : krotność.</param>
+        /// <param name="callback">Metoda która zostanie wywołana jako alarm</param>
+        /// <returns>ID Timera.</returns>
+        /****************************************************************************/
+        public static uint CreateTimer(uint alarm, int repeat, Timer.CallbackDelegate callback)
+        {
+            Timer timer = new Timer(TimeSpan.FromSeconds(alarm), repeat, callback);
+            Timers.Add(timer.ID, timer);
+            return timer.ID; 
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Release Timer
+        /****************************************************************************/
+        public static void ReleaseTimer(uint id)
+        {
+            Timers.Remove(id);
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Create Clock
+        /****************************************************************************/
+        public static Clock CreateClock()
+        {
+            Clock clock = new Clock();
+            Clocks.Add(clock);
+            return clock;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Create Clock (2)
+        /****************************************************************************/
+        public static Clock CreateClock(TimeSpan start,double ratio = 1)
+        {
+            Clock clock = new Clock(start,ratio);
+            Clocks.Add(clock);
+            return clock;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Create Clock (3)
+        /****************************************************************************/
+        public static Clock CreateClock(double ratio)
+        {
+            Clock clock = new Clock(TimeSpan.Zero, ratio);
+            Clocks.Add(clock);
+            return clock;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Release Clock
+        /****************************************************************************/
+        public static void ReleaseClock(Clock clock)
+        {
+            Clocks.Remove(clock);                
+        }
+        /****************************************************************************/
+
+    }
+    /********************************************************************************/  
+
+}
+/************************************************************************************/
