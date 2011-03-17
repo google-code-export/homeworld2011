ad6f97836f7b9e92377afde357bd0ddf43baf464
anddom03@gmail.com <anddom03@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-17 19:18:37 +0000

    Przeprowadzka paru plikow z input.component do input

---
+++ Zaraza/trunk/Plague/Plague/Input/ExtendedMouseMovementState.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace PlagueEngine.Input.Components
+{
+    struct ExtendedMouseMovementState
+    {
+        private Vector3 position;
+        private Vector3 oldPosition;
+        private Vector3 difference;//o ile zmienilo sie polozenie w x,y,z
+        private bool moved; //zmiana w x,y,z
+        private bool scrolled;//zmiana w z, tj bylo scrollniecie:D
+
+        public ExtendedMouseMovementState(int x,int y,int z,int oldX,int oldY,int oldZ)
+        {
+            moved = false;
+            scrolled = false;
+
+            position = new Vector3(x, y, z);
+            oldPosition = new Vector3(oldX, oldY, oldZ);
+            difference = position - oldPosition;
+            
+            if (difference.Length()!=0)
+            {
+                moved = true;
+            }
+
+            if (z != oldZ)
+            {
+                scrolled = true;
+            }
+            
+        }
+
+        public bool Scrolled
+        {
+            get { return this.scrolled; }
+        }
+        public Vector3 Position
+        {
+            get { return position; }
+        }
+
+        public Vector3 OldPosition
+        {
+            get { return oldPosition; }
+        }
+
+        public Vector3 Difference
+        {
+            get { return difference; }
+        }
+
+        public bool Moved
+        {
+            get { return moved; }
+        }
+
+    }
+}
