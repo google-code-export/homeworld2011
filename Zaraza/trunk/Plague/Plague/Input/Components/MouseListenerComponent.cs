a8582599f52369bb69d8e70f3038d2f4710e29e6
anddom03@gmail.com <anddom03@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-17 19:12:18 +0000

    Dodalem mouseListenera. Listenerem mozemy pobierac stany klawiszy myszy(prawy,lewy,srodkowy) oraz pozycje(scroll tutaj jest jako Z).

---
+++ Zaraza/trunk/Plague/Plague/Input/Components/MouseListenerComponent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+using PlagueEngine.LowLevelGameFlow;
+using PlagueEngine.Input;
+
+namespace PlagueEngine.Input.Components
+{
+    enum MouseKeyAction
+    {
+        LeftClick,RightClick,MiddleClick
+    }
+
+    enum MouseMoveAction
+    {
+        Move,Scroll
+    }
+
+    class MouseListenerComponent : GameObjectComponent
+    {
+        
+        private Input input = null;
+        private bool active = true;
+
+        public MouseListenerComponent(GameObjectInstance gameObject,Input input,bool active):base(gameObject)
+        {
+            this.input=input;
+            this.active=active;
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return active;
+            }
+
+            set
+            {
+                active = value;
+            }
+        }
+
+        public void subscribeKeys(onMouseKey oNmouseKey,params MouseKeyAction[] mouseKeyAction)
+        {
+            foreach (MouseKeyAction mouseKey in mouseKeyAction)
+            {
+                input.SubscribeMouseKeyListener(this, mouseKey, oNmouseKey);
+            }
+        }
+
+        public void subscribeMouseMove(onMouseMove onMouseMove, params MouseMoveAction[] mouseMoveAction)
+        {
+            foreach(MouseMoveAction mouseAction in mouseMoveAction)
+            {
+                input.SubscribeMouseMoveListener(this,mouseAction,onMouseMove);
+            }
+        }
+
+       
+        public override void ReleaseMe()
+        {
+            input.ReleaseMouseListenerComponent(this);
+        }
+
+    }
+}
