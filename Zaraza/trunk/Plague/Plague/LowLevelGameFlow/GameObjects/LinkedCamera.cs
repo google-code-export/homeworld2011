66c3b9a581abb9b1446aa8412c29cf0ef65a8038
anddom03@gmail.com <anddom03@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-19 17:15:03 +0000

    Poprawki linkedCamer'y

--- Zaraza/trunk/Plague/Plague/LowLevelGameFlow/GameObjects/LinkedCamera.cs
+++ Zaraza/trunk/Plague/Plague/LowLevelGameFlow/GameObjects/LinkedCamera.cs
@@ -17,6 +17,37 @@ using PlagueEngine.TimeControlSystem;
 namespace PlagueEngine.LowLevelGameFlow.GameObjects
 {
 
+
+    /************************************************************************************/
+    /// Regions
+    /************************************************************************************/
+    public struct regions
+    {
+        public region left, right, top, bottom;
+    }
+    /************************************************************************************/
+
+
+
+
+    /************************************************************************************/
+    /// Region
+    /************************************************************************************/
+    public struct region
+    {
+        public int x, y, width, height;
+        public region(int X, int Y, int Width, int Height)
+        {
+            x = X;
+            y = Y;
+            width = Width;
+            height = Height;
+        }
+    }
+    /************************************************************************************/
+
+
+
     /********************************************************************************/
     /// LinkedCamera
     /********************************************************************************/
@@ -38,6 +69,9 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
         private Vector3 target = Vector3.Zero;
 
         private Clock clock = TimeControl.CreateClock();
+
+        private bool shiftDown = false;
+        private regions mouseRegions;
         /****************************************************************************/
 
 
@@ -59,29 +93,153 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
 
             this.movementSpeed              = movementSpeed;
             this.rotationSpeed              = rotationSpeed;
+            this.zoomSpeed                  = zoomSpeed;
+            this.position                   = position;
+            this.target                     = target;
 
-            this.position = position;
-            this.target = target;
             this.World = Matrix.CreateLookAt(this.position, this.target, Vector3.Up);
 
             this.keyboardListenerComponent.SubscibeKeys(OnKey, Keys.W, Keys.S, Keys.A,
-                                                   Keys.D, Keys.Q, Keys.E);
+                                                   Keys.D, Keys.Q, Keys.E, Keys.LeftShift);
+
+            this.mouselistenerComponent.SubscribeMouseMove(onMouseMove,MouseMoveAction.Move,MouseMoveAction.Scroll);
 
 
-            
+            int screenWidth = cameraComponent.ScreenWidth();
+            int screenHeight = cameraComponent.ScreenHeight();
 
+            ///domyslne miejsca na ekranie do przesuwania mysza kamery
+            mouseRegions.left = new region(0, 0, (int)(screenWidth * 0.05), screenHeight);
+            mouseRegions.right = new region((int)(screenWidth * 0.95), 0, (int)(screenWidth * 0.05), screenHeight);
+            mouseRegions.top = new region(0, 0, screenWidth, (int)(screenHeight * 0.05));
+            mouseRegions.bottom = new region(0, (int)(screenHeight * 0.95), screenWidth, (int)(screenHeight * 0.05));
+       
         }
         /****************************************************************************/
-    
+
+
+
+        /****************************************************************************/
+        /// Regions
+        /****************************************************************************/
+        public regions Regions
+        {
+            get { return mouseRegions; }
+            set { this.mouseRegions = value; }
+        }
+        /****************************************************************************/
+
+
+        /************************************************************************************/
+        /// Is Mouse In Region
+        /************************************************************************************/
+        private bool isMouseInRegion(region testedRegion, ExtendedMouseMovementState mouseState)
+        {
+
+            if ((mouseState.Position.X >= testedRegion.x) && (mouseState.Position.Y >= testedRegion.y) && (mouseState.Position.X <= (testedRegion.x + testedRegion.width)) && (mouseState.Position.Y <= (testedRegion.y + testedRegion.height)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /************************************************************************************/
+
+
+
+
+        /****************************************************************************/
+        /// On Mouse Move 
+        /****************************************************************************/
+        private void onMouseMove(MouseMoveAction mouseMoveAction, ExtendedMouseMovementState mouseMoveState)
+        {
+            Vector3 direction = Vector3.Normalize(target - position);
+            
+
+            Vector3 perpendicular = Vector3.Transform(direction, Matrix.CreateRotationY(MathHelper.ToRadians(90.0f)));
+            perpendicular = Vector3.Normalize(perpendicular);
+
+            float time = (float)(clock.DeltaTime.TotalMilliseconds);
+
+
+            switch (mouseMoveAction)
+            {
+                case MouseMoveAction.Scroll:
+
+                    Vector3 testPosition = position + direction * zoomSpeed * time * mouseMoveState.ScrollDifference;
+                    float distance = Vector3.Distance(target, testPosition);
+
+                    if (distance>100 && testPosition.Y>target.Y)//kamera musi patrzec z gory w dol, nie odwrotnie. wiec lepiej jej nie przyblizac za duzo
+                    {
+
+                        if (shiftDown)
+                        {
+                            position.Y += zoomSpeed * time * direction.Y *mouseMoveState.ScrollDifference ;
+                        }
+                        else
+                        {
+                            position += zoomSpeed * time * direction * mouseMoveState.ScrollDifference;
+                        }
+
+                    }
+
+                    break;
+
+
+                case MouseMoveAction.Move:
+
+                    direction.Y = 0;
+                    perpendicular.Y = 0;
+                    if (isMouseInRegion(mouseRegions.left, mouseMoveState))
+                    {
+                        position += perpendicular * movementSpeed * time;
+                        target += perpendicular * movementSpeed * time;
+                    }
+
+                    if (isMouseInRegion(mouseRegions.right, mouseMoveState))
+                    {
+                        position -= perpendicular * movementSpeed * time;
+                        target -= perpendicular * movementSpeed * time;
+
+                    }
+
+                    if (isMouseInRegion(mouseRegions.top, mouseMoveState))
+                    {
+                        position += direction * movementSpeed * time;
+                        target += direction * movementSpeed * time;
+
+                    }
+
+                    if (isMouseInRegion(mouseRegions.bottom, mouseMoveState))
+                    {
+                        position -= direction * movementSpeed * time;
+                        target -= direction * movementSpeed * time;
+
+                    }
+
+                    break;
+            }
+
+
+            cameraComponent.LookAt(position, target, Vector3.Up);
+        }
+        /****************************************************************************/
+
+
+
 
         /****************************************************************************/
         /// On Key
         /****************************************************************************/
         private void OnKey(Keys key, ExtendedKeyState state)
         {
-            if (!state.IsDown()) return;
+            if (key == Keys.LeftShift)
+            {
+                shiftDown = state.IsDown();
+            }
 
-            Vector3 Up = Vector3.Up;
+            if (!state.IsDown()) return;
 
             Vector3 direction = Vector3.Normalize(target - position);
             direction.Y = 0;
@@ -90,39 +248,41 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
             perpendicular = Vector3.Normalize(perpendicular);
             perpendicular.Y = 0;
 
+            float time = (float)(clock.DeltaTime.TotalMilliseconds);
+
             switch (key)
             {
                 case Keys.W:
-                            position += direction * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
-                            target += direction * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
+                            position += direction * time * movementSpeed ;
+                            target += direction * time * movementSpeed ;
                             
                             
                             break;
 
                 case Keys.S:
-                            position -= direction * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
-                            target -= direction * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
+                            position -= direction * time * movementSpeed ;
+                            target -= direction * time * movementSpeed ;
                             
                             
                             break;
 
                 case Keys.A:
-                            position += perpendicular * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
-                            target += perpendicular * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
+                            position += perpendicular * time * movementSpeed ;
+                            target += perpendicular * time * movementSpeed ;
                             
                             
                             break;
 
                 case Keys.D:
-                            position -= perpendicular * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
-                            target -= perpendicular * (float)(movementSpeed * clock.DeltaTime.TotalMilliseconds);
+                            position -= perpendicular * time * movementSpeed ;
+                            target -= perpendicular * time * movementSpeed ;
                             
                             
                             break;
 
                 case Keys.Q:
                     position -= target;
-                    position = Vector3.Transform(position, Matrix.CreateRotationY((float)(rotationSpeed * clock.DeltaTime.TotalMilliseconds)));
+                    position = Vector3.Transform(position, Matrix.CreateRotationY(time* rotationSpeed ));
                     position += target;
                     
                     
@@ -130,15 +290,18 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
 
                 case Keys.E:
                     position -= target;
-                    position = Vector3.Transform(position, Matrix.CreateRotationY((float)(rotationSpeed * clock.DeltaTime.TotalMilliseconds)*-1));
+                    position = Vector3.Transform(position, Matrix.CreateRotationY(time* rotationSpeed *-1));
                     position += target;
                     
                     
                     break;
+                
+            
+
 
 
             }
-            cameraComponent.LookAt(ref position, ref target, ref Up);
+            cameraComponent.LookAt(position,target, Vector3.Up);
         }
         /****************************************************************************/
 
