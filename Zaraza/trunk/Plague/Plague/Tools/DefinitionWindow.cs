59ec6224f4ecbabe804c7f8835a00e1c85fe2f68
anddom03@gmail.com <anddom03@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-03-24 18:07:12 +0000

    Dzialajacy edytor definicji:) zdarza mu sie rzucac bledem, nie wiem jeszcze dlaczego. moze to kobieta ;)

--- Zaraza/trunk/Plague/Plague/Tools/DefinitionWindow.cs
+++ Zaraza/trunk/Plague/Plague/Tools/DefinitionWindow.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Reflection;
 
+using PlagueEngine.LowLevelGameFlow;
 
 namespace PlagueEngine.Tools
 {
@@ -16,6 +17,7 @@ namespace PlagueEngine.Tools
         public class Field
         {
             public CheckBox checkbox = new CheckBox();
+            public Label value = new Label();
             public Label label = new Label();
         }
 
@@ -24,46 +26,69 @@ namespace PlagueEngine.Tools
         public Button create = new Button();
         public Button cancel = new Button();
         public TextBox textbox = new TextBox();
+        public Label definitionName = new Label();
 
         public bool canceled = false;
 
-        public DefinitionWindow(List<PropertyInfo> fieldInfo)
+        public GameObjectInstanceData currentObject = null;
+        public DefinitionWindow(List<PropertyInfo> PropertyInfo, GameObjectInstanceData currentObject)
         {
             InitializeComponent();
 
-            this.Size = new Size(260, 90 + fieldInfo.Count * 25);
+            this.currentObject = currentObject;
+
+            this.Size = new Size(300, 90 + PropertyInfo.Count * 25);
 
             this.create.Text = "Create";
             this.cancel.Text = "Cancel";
+            this.definitionName.Text = "Name:";
 
-            this.textbox.Size = new Size(160,20);
+            this.textbox.Size = new Size(200,20);
+            this.definitionName.Size = new Size(40, 20);
 
-            this.textbox.Location = new Point(60, 10);
-            this.create.Location = new Point(20, 35 + fieldInfo.Count  * 25);
-            this.cancel.Location = new Point(150, 35 + fieldInfo.Count * 25);
+            
+            this.definitionName.Location = new Point(20, 10);
+            this.textbox.Location = new Point(90, 10);
+            this.create.Location = new Point(20, 35 + PropertyInfo.Count * 25);
+            this.cancel.Location = new Point(150, 35 + PropertyInfo.Count * 25);
 
             this.create.Click += new EventHandler(Create_Click);
             this.cancel.Click += new EventHandler(Cancel_Click);
 
+            Controls.Add(definitionName);
             Controls.Add(create);
             Controls.Add(cancel);
             Controls.Add(textbox);
 
 
-            for (int i = 0; i < fieldInfo.Count; i++)
+            for (int i = 0; i < PropertyInfo.Count; i++)
             {
                 
                     Field field = new Field();
 
-                    field.label.Text = fieldInfo[i].Name;
+                    field.label.Text = PropertyInfo[i].Name;
                     field.label.Location = new Point(20, 35 + 25 * i);
                     field.label.Size = new Size(130, 20);
 
-                    field.checkbox.Location = new Point(190, 35 + 25 * i);
+                    try//nullReferenceExcepton? :O
+                    {
+                        field.value.Text = PropertyInfo[i].GetValue(currentObject, null).ToString();
+                    }
+                    catch
+                    {
+                        field.value.Text = "";
+                    }
+
+
+                    field.value.Location = new Point(180, 35 + 25 * i);
+                    field.value.Size = new Size(80, 20);
+                    
+                    field.checkbox.Location = new Point(270, 35 + 25 * i);
                     field.checkbox.Checked = false;
 
                     Controls.Add(field.label);
                     Controls.Add(field.checkbox);
+                    Controls.Add(field.value);
 
                     fields.Add(field);
                 
@@ -72,7 +97,10 @@ namespace PlagueEngine.Tools
 
         private void Create_Click(object sender, System.EventArgs e)
         {
-            this.Hide();
+            if (this.textbox.Text != string.Empty)
+            {
+                this.Hide();
+            }
         }
         private void Cancel_Click(object sender, System.EventArgs e)
         {
