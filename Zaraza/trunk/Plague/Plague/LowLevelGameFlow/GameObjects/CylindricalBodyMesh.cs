ec8e87e2613202b410df547af586a5817bfbea6a
anddom03@gmail.com <anddom03@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-04-10 17:59:00 +0000

    Brakujace pliczki z fizyki

---
+++ Zaraza/trunk/Plague/Plague/LowLevelGameFlow/GameObjects/CylindricalBodyMesh.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.ComponentModel;
+
+using PlagueEngine.LowLevelGameFlow;
+using PlagueEngine.Rendering.Components;
+using PlagueEngine.Rendering;
+using PlagueEngine.Physics;
+using PlagueEngine.Physics.Components;
+
+
+/********************************************************************************/
+/// PlagueEngine.LowLevelGameFlow.GameObjects
+/********************************************************************************/
+namespace PlagueEngine.LowLevelGameFlow.GameObjects
+{
+
+    /********************************************************************************/
+    /// CylindricalBodyMesh
+    /********************************************************************************/
+    class CylindricalBodyMesh : GameObjectInstance
+    {
+
+        /********************************************************************************/
+        /// Fields
+        /********************************************************************************/
+        MeshComponent meshComponent = null;
+        CylindricalBodyComponent physicsComponent = null;
+        /********************************************************************************/
+
+
+
+
+        /********************************************************************************/
+        /// Init
+        /********************************************************************************/
+        public void Init(MeshComponent meshComponent, CylindricalBodyComponent physcisComponent, Matrix world)
+        {
+            this.meshComponent = meshComponent;
+            this.physicsComponent = physcisComponent;
+            this.World = world;
+        }
+        /********************************************************************************/
+
+
+
+
+
+        /********************************************************************************/
+        /// Release Components
+        /********************************************************************************/
+        public override void ReleaseComponents()
+        {
+            meshComponent.ReleaseMe();
+            physicsComponent.ReleaseMe();
+        }
+        /********************************************************************************/
+
+
+
+
+        /********************************************************************************/
+        /// GetData
+        /********************************************************************************/
+        public override GameObjectInstanceData GetData()
+        {
+            CylindricalBodyMeshData data = new CylindricalBodyMeshData();
+            GetData(data);
+            data.Model = meshComponent.Model.Name;
+            data.Diffuse = (meshComponent.Textures.Diffuse == null ? String.Empty : meshComponent.Textures.Diffuse.Name);
+            data.Specular = (meshComponent.Textures.Specular == null ? String.Empty : meshComponent.Textures.Specular.Name);
+            data.Normals = (meshComponent.Textures.Normals == null ? String.Empty : meshComponent.Textures.Normals.Name);
+
+            data.InstancingMode = Renderer.InstancingModeToUInt(meshComponent.InstancingMode);
+
+            data.Mass = physicsComponent.Mass;
+            data.Elasticity = physicsComponent.Elasticity;
+            data.StaticRoughness = physicsComponent.StaticRoughness;
+            data.DynamicRoughness = physicsComponent.DynamicRoughness;
+            data.Lenght = physicsComponent.Length;
+            data.Radius = physicsComponent.Radius;
+            data.Immovable = physicsComponent.Immovable;
+
+            return data;
+        }
+        /********************************************************************************/
+
+
+
+    }
+    /********************************************************************************/
+
+
+    /********************************************************************************/
+    /// CylindricalBodyMeshData
+    /********************************************************************************/
+    [Serializable]
+    public class CylindricalBodyMeshData : GameObjectInstanceData
+    {
+        [CategoryAttribute("Model")]
+        public String Model { get; set; }
+
+        [CategoryAttribute("Textures")]
+        public String Diffuse { get; set; }
+
+        [CategoryAttribute("Textures")]
+        public String Specular { get; set; }
+
+        [CategoryAttribute("Textures")]
+        public String Normals { get; set; }
+
+        [CategoryAttribute("Instancing"),
+        DescriptionAttribute("1 - No Instancing, 2 - Static Instancing, 3 - Dynamic Instancing.")]
+        public uint InstancingMode { get; set; }
+
+        [CategoryAttribute("Physics")]
+        public float Mass { get; set; }
+
+        [CategoryAttribute("Physics")]
+        public float Elasticity { get; set; }
+
+        [CategoryAttribute("Physics")]
+        public float StaticRoughness { get; set; }
+
+        [CategoryAttribute("Physics")]
+        public float DynamicRoughness { get; set; }
+
+        [CategoryAttribute("Physics")]
+        public bool Immovable { get; set; }
+
+        [CategoryAttribute("Collision Skin")]
+        public float Radius { get; set; }
+
+        [CategoryAttribute("Collision Skin")]
+        public float Lenght { get; set; }
+
+
+    }
+    /********************************************************************************/
+
+
+
+}
+/********************************************************************************/
---
+++ Zaraza/trunk/Plague/Plague/Physics/CollisionSkinComponent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+using JigLibX.Geometry;
+using JigLibX.Collision;
+using JigLibX.Physics;
+using JigLibX.Math;
+using PlagueEngine.LowLevelGameFlow;
+
+namespace PlagueEngine.Physics
+{
+
+    class CollisionSkinComponent : GameObjectComponent
+    {
+        protected CollisionSkin skin = null;
+
+        private MaterialProperties material;
+        private bool isEnabled = false;
+
+        internal static PhysicsManager physicsManager = null;
+
+        public CollisionSkinComponent(GameObjectInstance gameObject, MaterialProperties material)
+            : base(gameObject)
+        {
+            skin = new CollisionSkin();
+            this.material = material;
+
+            physicsManager.collisionSkins.Add(this);
+        }
+
+
+
+        /****************************************************************************/
+        /// Enable
+        /****************************************************************************/
+        public void Enable()
+        {
+            PhysicsSystem.CurrentPhysicsSystem.CollisionSystem.AddCollisionSkin(skin);
+            isEnabled = true;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Disable
+        /****************************************************************************/
+        public void Disable()
+        {
+            PhysicsSystem.CurrentPhysicsSystem.CollisionSystem.RemoveCollisionSkin(skin);
+            isEnabled = false;
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// ReleaseMe
+        /****************************************************************************/
+        public override void ReleaseMe()
+        {
+            if (isEnabled) Disable();
+            physicsManager.collisionSkins.Remove(this);
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Properties
+        /****************************************************************************/
+        public bool IsEnabled { get { return isEnabled; } }
+        public float Elasticity { get { return material.Elasticity; } }
+        public float StaticRoughness { get { return material.StaticRoughness; } }
+        public float DynamicRoughness { get { return material.DynamicRoughness; } }
+
+        public CollisionSkin Skin { get { return skin; } }
+        /****************************************************************************/
+    }
+}
