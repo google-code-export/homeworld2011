0d70804c1269af503d62cb521dba6e4155cb29bf
bartosz.unobomber@gmail.com <bartosz.unobomber@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-04-12 00:20:22 +0000

    Rozpoczynam prace nad GUI. Pierwszy komponent już chyba poprawny i ok.

---
+++ Zaraza/trunk/Plague/Plague/GUI/GUIComponentsFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlagueEngine.GUI
+{
+    class GUIComponentsFactory
+    {
+        /****************************************************************************/
+        /// Fields
+        /****************************************************************************/
+        //private Renderer        renderer    = null;
+        //private ContentManager  content     = null;
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// Constructor
+        /****************************************************************************/
+        public GUIComponentsFactory()
+        {
+            //this.renderer   = renderer;
+            //this.content    = renderer.contentManager;
+        }
+        /****************************************************************************/
+
+    }
+}
