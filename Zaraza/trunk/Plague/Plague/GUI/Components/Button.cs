3e081627628473407dfb159b9392effe60118fbb
bartosz.unobomber@gmail.com <bartosz.unobomber@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-04-12 00:20:22 +0000

    Rozpoczynam prace nad GUI. Pierwszy komponent już chyba poprawny i ok.

---
+++ Zaraza/trunk/Plague/Plague/GUI/Components/Button.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Nuclex.Input;
+using Nuclex.UserInterface.Controls.Desktop;
+using Nuclex.UserInterface;
+
+
+
+
+/************************************************************************************/
+/// PlagueEngine.GUI.Components
+/************************************************************************************/
+namespace PlagueEngine.GUI.Components
+{
+
+    /********************************************************************************/
+    /// GUI Button Component
+    /********************************************************************************/
+    class Button 
+    {
+        /****************************************************************************/
+        /// Fields
+        /****************************************************************************/
+        public ButtonControl button {  get; private set; }
+        /****************************************************************************/
+        
+        /****************************************************************************/
+        /// Constructor
+        /****************************************************************************/
+        public Button(Screen screen, String text)
+        {
+            button = new ButtonControl();
+            button.Text = text;
+            button.Bounds = new UniRectangle(new UniScalar(1.0f, -190.0f), new UniScalar(1.0f, -32.0f), 100, 32);
+            //screen.Desktop.Children.Add(button);  
+        }
+
+        public void setDelegate(EventHandler handler)
+        {
+            button.Pressed += handler;
+        }
+
+
+    }
+}
