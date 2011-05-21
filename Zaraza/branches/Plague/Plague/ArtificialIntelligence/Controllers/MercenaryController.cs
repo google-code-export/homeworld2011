8ba0ddada6024ae43a05430a9e78b9ba9f024caf
bartosz.unobomber@gmail.com <bartosz.unobomber@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-05-21 23:51:33 +0000

    Commit "przed snem". W zasadzie to niezawiele sie pozmienialo, sporo czyszczenia, czyszczenia, czyszczenia...

--- Zaraza/branches/Plague/Plague/ArtificialIntelligence/Controllers/MercenaryController.cs
+++ Zaraza/branches/Plague/Plague/ArtificialIntelligence/Controllers/MercenaryController.cs
@@ -12,167 +12,84 @@ enum MoveState{STOP, MOVE, FOLLOW, TO_GRAB};
 namespace PlagueEngine.ArtificialIntelligence.Controllers
 {
     class MercenaryController : Controller
-
     {
 
-        public LivingBeing being { get; private set; }
-        
-        private GameObjectInstance objectTarget;
-        private TacticalAction moving;
-        private Vector3 target;
-
-        private float rotationSpeed = 0;
-        private float movingSpeed = 0;
-        private float distance = 0;
-        private float anglePrecision = 0;
 
         private GameObjectInstance currentObject;
 
-        //TODO: change temporary constructor.
+        //TODO: change temporary constructor
         public MercenaryController(LivingBeing person, float rotationSpeed,
                          float movingSpeed,
                          float distance,
                          float angle)
         {
-            this.being = person;
+            this.GameObject = person;
             this.rotationSpeed = rotationSpeed;
             this.movingSpeed = movingSpeed;
             this.distance = distance;
             this.anglePrecision = angle;
         }
 
-
-        public  void OnEvent(EventsSystem.EventsSender sender, EventArgs e)
+        override public bool OnEvent(EventsSystem.EventsSender sender, EventArgs e)
         {
-
-            #region Move to point (1)
-            /*************************************/
-            /// MoveToPointCommandEvent
-            /*************************************/
-            if (e.GetType().Equals(typeof(MoveToPointCommandEvent)))
+            if (base.OnEvent(sender, e))
             {
-                MoveToPointCommandEvent moveToPointCommandEvent = e as MoveToPointCommandEvent;
-
-                target = moveToPointCommandEvent.point;
-                moving = TacticalAction.MOVE;
+                return true;
             }
-            #endregion
-
-            #region Grab Object (2)
-            /*************************************/
-            /// GrabObjectCommandEvent
-            /*************************************/
             else if (e.GetType().Equals(typeof(GrabObjectCommandEvent)))
             {
-                GrabObjectCommandEvent moveToObjectCommandEvent = e as GrabObjectCommandEvent;
-
-                if (moveToObjectCommandEvent.gameObject != this.being)
-                {
-                    objectTarget = moveToObjectCommandEvent.gameObject;
-                    moving = TacticalAction.GRAB;
-                    being.Body.SubscribeCollisionEvent(objectTarget.ID);
-                }
+                grabEvent(e);
+                return true;
             }
-            #endregion
-
-            #region Collision detected (ends 2 & 4)
-            /*************************************/
-            /// CollisionEvent
-            /*************************************/
             else if (e.GetType().Equals(typeof(CollisionEvent)))
             {
-                CollisionEvent collisionEvent = e as CollisionEvent;
-
-                if (collisionEvent.gameObject == objectTarget)
-                {
-                    being.Body.CancelSubscribeCollisionEvent(objectTarget.ID);
-                    if (moving == TacticalAction.GRAB)
-                    {
-                        #region Pick object if pickable
-                        if (objectTarget.Status == GameObjectStatus.Pickable)
-                        {
-                            #region Drop current object if present
-                            if (currentObject != null)
-                            {
-                                currentObject.World.Translation += Vector3.Normalize(being.World.Forward) * 2;
-                                currentObject.Owner = null;
-                                currentObject.OwnerBone = -1;
-                            }
-                            #endregion
-
-                            currentObject = objectTarget;
-                            objectTarget.Owner = this.being;
-                            objectTarget.OwnerBone = being.Mesh.BoneMap[being.gripBone];
-                        }
-                        #endregion
-                    }
-                    else if (moving == TacticalAction.EXAMINE)
-                    {
-                        #region Examine object
-                        //TODO: zmienić na wysyłkę przez kontroler, nie przez GO
-                        being.SendEvent(new ExamineEvent(), EventsSystem.Priority.Normal, objectTarget);
-                        #endregion
-                    }
-                    #region Turn to idle
-                    objectTarget = null;
-                    moving = 0;
-                    being.Controller.StopMoving();
-                    being.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
-                    #endregion
-                }
+                collisionEvent(e);
+                return true;
             }
-            #endregion
-
-            #region Follow object (3)
-            /*************************************/
-            /// FollowObjectCommandEvent
-            /*************************************/
-            else if (e.GetType().Equals(typeof(FollowObjectCommandEvent)))
-            {
-                FollowObjectCommandEvent FollowObjectCommandEvent = e as FollowObjectCommandEvent;
-
-                if (FollowObjectCommandEvent.gameObject != this.being)
-                {
-                    objectTarget = FollowObjectCommandEvent.gameObject;
-                    moving = TacticalAction.FOLLLOW;
-                }
-            }
-            #endregion
-
-            #region Examine Object (4)
-            /*************************************/
-            /// ExamineObjectCommandEvent
-            /*************************************/
             else if (e.GetType().Equals(typeof(ExamineObjectCommandEvent)))
             {
-                ExamineObjectCommandEvent ExamineObjectCommandEvent = e as ExamineObjectCommandEvent;
-
-                objectTarget = ExamineObjectCommandEvent.gameObject;
-                moving = TacticalAction.EXAMINE;
-                being.Body.SubscribeCollisionEvent(objectTarget.ID);
+                examineEvent(e);
+                return true;
             }
-            #endregion
+            return false;
         }
 
+        public override void Update(TimeSpan deltaTime)
+        {
+            switch (action)
+            {
+                case TacticalAction.GRAB:
+                case TacticalAction.EXAMINE:
+                    grabOrExamine(deltaTime);
+                    break;
+                case TacticalAction.DROP:
+                    drop();
+                    break;
+                default:
+                    base.Update(deltaTime);
+                    break;
+            }
+        }
 
-        
-
+        /****************************************************************************/
+        /// ACTIONS HANDLING
+        /****************************************************************************/
         private void grabOrExamine(TimeSpan deltaTime)
         {
             if (objectTarget.IsDisposed() || objectTarget.Owner != null)
             {
                 #region Cancel Grab or Examine
                 objectTarget = null;
-                moving = 0;
-                being.Controller.StopMoving();
-                being.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
+                action = TacticalAction.IDLE;
+                GameObject.Controller.StopMoving();
+                GameObject.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
                 #endregion
                 return;
             }
 
-            Vector3 direction = being.World.Translation - objectTarget.World.Translation;
+            Vector3 direction = GameObject.World.Translation - objectTarget.World.Translation;
             Vector2 v1 = Vector2.Normalize(new Vector2(direction.X, direction.Z));
-            Vector2 v2 = Vector2.Normalize(new Vector2(being.World.Forward.X, being.World.Forward.Z));
+            Vector2 v2 = Vector2.Normalize(new Vector2(GameObject.World.Forward.X, GameObject.World.Forward.Z));
 
             float det = v1.X * v2.Y - v1.Y * v2.X;
             float angle = (float)Math.Acos((double)Vector2.Dot(v1, v2));
@@ -184,123 +101,97 @@ namespace PlagueEngine.ArtificialIntelligence.Controllers
 
             if (Math.Abs(angle) > anglePrecision)
             {
-                being.Controller.Rotate(MathHelper.ToDegrees(angle) * rotationSpeed * (float)deltaTime.TotalSeconds);
+                GameObject.Controller.Rotate(MathHelper.ToDegrees(angle) * rotationSpeed * (float)deltaTime.TotalSeconds);
             }
 
-            being.Controller.MoveForward(movingSpeed * (float)deltaTime.TotalSeconds);
+            GameObject.Controller.MoveForward(movingSpeed * (float)deltaTime.TotalSeconds);
 
             #region Blend to Run
-            if (being.Mesh.CurrentClip != "Run")
+            if (GameObject.Mesh.CurrentClip != "Run")
             {
-                being.Mesh.BlendTo("Run", TimeSpan.FromSeconds(0.5f));
+                GameObject.Mesh.BlendTo("Run", TimeSpan.FromSeconds(0.5f));
             }
             #endregion
         }
 
-        private void follow(TimeSpan deltaTime)
+        private void drop()
         {
-            if (objectTarget.IsDisposed())
+            if (currentObject != null)
             {
-                #region Turn to Idle
-                objectTarget = null;
-                moving = 0;
-                being.Controller.StopMoving();
-                being.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
-                #endregion
-                return;
+                currentObject.World.Translation += Vector3.Normalize(GameObject.World.Forward) * 2;
+                currentObject.Owner = null;
+                currentObject.OwnerBone = -1;
             }
-            else
+            action = TacticalAction.IDLE;
+        }
+
+        /****************************************************************************/
+        /// EVENTS HANDLING
+        /****************************************************************************/
+        private void grabEvent(EventArgs e)
+        {
+            GrabObjectCommandEvent moveToObjectCommandEvent = e as GrabObjectCommandEvent;
+
+            if (moveToObjectCommandEvent.gameObject != this.GameObject)
             {
-                double currentDistance = Vector2.Distance(new Vector2(being.World.Translation.X, being.World.Translation.Z),
-                                     new Vector2(objectTarget.World.Translation.X, objectTarget.World.Translation.Z));
-                if (being.Mesh.CurrentClip == "Idle" && currentDistance > 8)
+                objectTarget = moveToObjectCommandEvent.gameObject;
+                action = TacticalAction.GRAB;
+                GameObject.Body.SubscribeCollisionEvent(objectTarget.ID);
+            }
+        }
+
+        private void collisionEvent(EventArgs e)
+        {
+            CollisionEvent collisionEvent = e as CollisionEvent;
+
+            if (collisionEvent.gameObject == objectTarget)
+            {
+                GameObject.Body.CancelSubscribeCollisionEvent(objectTarget.ID);
+                if (action == TacticalAction.GRAB)
                 {
-                    #region Resume Chase
-                    Vector3 direction = being.World.Translation - objectTarget.World.Translation;
-                    Vector2 v1 = Vector2.Normalize(new Vector2(direction.X, direction.Z));
-                    Vector2 v2 = Vector2.Normalize(new Vector2(being.World.Forward.X, being.World.Forward.Z));
-
-                    float det = v1.X * v2.Y - v1.Y * v2.X;
-                    float angle = (float)Math.Acos((double)Vector2.Dot(v1, v2));
-
-                    if (det < 0)
+                    #region Pick object if pickable
+                    if (objectTarget.Status == GameObjectStatus.Pickable)
                     {
-                        angle = -angle;
+                        #region Drop current object if present
+                        if (currentObject != null)
+                        {
+                            currentObject.World.Translation += Vector3.Normalize(GameObject.World.Forward) * 2;
+                            currentObject.Owner = null;
+                            currentObject.OwnerBone = -1;
+                        }
+                        #endregion
+
+                        currentObject = objectTarget;
+                        objectTarget.Owner = this.GameObject;
+                        objectTarget.OwnerBone = GameObject.Mesh.BoneMap[GameObject.gripBone];
                     }
-
-                    if (Math.Abs(angle) > anglePrecision)
-                    {
-                        being.Controller.Rotate(MathHelper.ToDegrees(angle) * rotationSpeed * (float)deltaTime.TotalSeconds);
-                    }
-
-                    being.Controller.MoveForward(movingSpeed * (float)deltaTime.TotalSeconds);
-
-                    being.Mesh.BlendTo("Run", TimeSpan.FromSeconds(0.3f));
-
                     #endregion
                 }
-                else if (being.Mesh.CurrentClip != "Idle")
+                else if (action == TacticalAction.EXAMINE)
                 {
-                    if (currentDistance < 4)
-                    {
-                        #region Pause Chase
-                        being.Controller.StopMoving();
-                        being.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
-                        #endregion
-                        return;
-                    }
-                    else
-                    {
-                        #region Continue Running
-                        Vector3 direction = being.World.Translation - objectTarget.World.Translation;
-                        Vector2 v1 = Vector2.Normalize(new Vector2(direction.X, direction.Z));
-                        Vector2 v2 = Vector2.Normalize(new Vector2(being.World.Forward.X, being.World.Forward.Z));
-
-                        float det = v1.X * v2.Y - v1.Y * v2.X;
-                        float angle = (float)Math.Acos((double)Vector2.Dot(v1, v2));
-
-                        if (det < 0)
-                        {
-                            angle = -angle;
-                        }
-
-                        if (Math.Abs(angle) > anglePrecision)
-                        {
-                            being.Controller.Rotate(MathHelper.ToDegrees(angle) * rotationSpeed * (float)deltaTime.TotalSeconds);
-                        }
-
-                        being.Controller.MoveForward(movingSpeed * (float)deltaTime.TotalSeconds);
-
-                        if (being.Mesh.CurrentClip != "Run")
-                        {
-                            being.Mesh.BlendTo("Run", TimeSpan.FromSeconds(0.3f));
-                        }
-                        #endregion
-                    }
+                    #region Examine object
+                    //TODO: zmienić na wysyłkę przez kontroler, nie przez GO
+                    GameObject.SendEvent(new ExamineEvent(), EventsSystem.Priority.Normal, objectTarget);
+                    #endregion
                 }
+                #region Turn to idle
+                objectTarget = null;
+                action = 0;
+                GameObject.Controller.StopMoving();
+                GameObject.Mesh.BlendTo("Idle", TimeSpan.FromSeconds(0.3f));
+                #endregion
             }
         }
 
-
-        /****************************************************************************/
-        /// Update
-        /****************************************************************************/
-        public  void Update(TimeSpan deltaTime)
+        private void examineEvent(EventArgs e)
         {
-            switch (moving)
-            {
-                case TacticalAction.MOVE:
-                    move(deltaTime);
-                    break;
-                case TacticalAction.GRAB:
-                case TacticalAction.EXAMINE:
-                    grabOrExamine(deltaTime);
-                    break;
-                case TacticalAction.FOLLLOW:
-                    follow(deltaTime);
-                    break;
-            }
+            ExamineObjectCommandEvent ExamineObjectCommandEvent = e as ExamineObjectCommandEvent;
+
+            objectTarget = ExamineObjectCommandEvent.gameObject;
+            action = TacticalAction.EXAMINE;
+            GameObject.Body.SubscribeCollisionEvent(objectTarget.ID);
         }
-        /****************************************************************************/
+
+        /*****************************************************************************************/
     }
 }
