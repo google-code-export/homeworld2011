46b1f9d9d4b18383c3f2b6723178230b3e42eeb1
lukaszm89@gmail.com <lukaszm89@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-06-03 20:49:36 +0000

    Poprawka usuwająca przenikanie przez ściany i inne obiekty. Nadal zdarzy się że będą się wspinać np na ciężarówkę czy inne podone rzeczy ale już przez nią nie przenikną;)

--- Zaraza/trunk/Plague/Plague/Physics/PhysicsManager.cs
+++ Zaraza/trunk/Plague/Plague/Physics/PhysicsManager.cs
@@ -1,20 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
 using JigLibX.Physics;
 using JigLibX.Collision;
-using PlagueEngine.Physics;
-using PlagueEngine.Physics.Components;
-using PlagueEngine.LowLevelGameFlow;
-using PlagueEngine.LowLevelGameFlow.GameObjects;
 using PlagueEngine.Resources;
 
-
-/************************************************************************************/
-///  PlagueEngine.Physics
-/************************************************************************************/
 namespace PlagueEngine.Physics
 {
 
@@ -27,11 +15,11 @@ namespace PlagueEngine.Physics
         /****************************************************************************/
         ///  Fields
         /****************************************************************************/
-        private  PhysicsSystem                              physicsSystem            = null;
+        private  PhysicsSystem                              physicsSystem;
         internal Dictionary<int, RigidBodyComponent>        rigidBodies              = new Dictionary<int, RigidBodyComponent>();
         internal Dictionary<int, CollisionSkinComponent>    collisionSkins           = new Dictionary<int, CollisionSkinComponent>();
         internal List<PhysicsController>                    controllers              = new List<PhysicsController>();
-        internal PhysicsComponentFactory                    physicsComponentFactory  = null;
+        internal PhysicsComponentFactory                    physicsComponentFactory;
         /****************************************************************************/
 
 
@@ -40,24 +28,17 @@ namespace PlagueEngine.Physics
         /****************************************************************************/
         public PhysicsManager(ContentManager content)
         {                       
-
-
             physicsComponentFactory       = new PhysicsComponentFactory(content);
-            physicsSystem                 = new PhysicsSystem();
-
-            //physicsSystem.CollisionSystem = new CollisionSystemBrute();
-
-            physicsSystem.CollisionSystem = new CollisionSystemSAP();
-
-            physicsSystem.SolverType = PhysicsSystem.Solver.Fast;
-            physicsSystem.EnableFreezing                = true;
-            //physicsSystem.IsShockStepEnabled            = true;
+            physicsSystem                 = new PhysicsSystem
+                                                {
+                                                    CollisionSystem = new CollisionSystemSAP(),
+                                                    SolverType = PhysicsSystem.Solver.Fast,
+                                                    EnableFreezing = true,
+                                                    NumCollisionIterations = 5,
+                                                    NumContactIterations = 5,
+                                                    NumPenetrationRelaxtionTimesteps = 5
+                                                };
             physicsSystem.CollisionSystem.UseSweepTests = true;
-            
-            physicsSystem.NumCollisionIterations           = 1;
-            physicsSystem.NumContactIterations             = 1;
-            physicsSystem.NumPenetrationRelaxtionTimesteps = 1;
-
             RigidBodyComponent.physicsManager = this;
             CollisionSkinComponent.physicsManager = this;
             PhysicsUlitities.collisionSystem = physicsSystem.CollisionSystem;
@@ -74,15 +55,10 @@ namespace PlagueEngine.Physics
             if (timeStep < 1.0f / 60.0f) physicsSystem.Integrate(timeStep);
             else physicsSystem.Integrate(1.0f / 60.0f);
 
-            //PhysicsSystem.CurrentPhysicsSystem.Integrate(timeStep);
-
-
-
             foreach (RigidBodyComponent rigidBody in rigidBodies.Values)
             {
                 rigidBody.Update();
             }
-
 
             foreach (CollisionSkinComponent skin in collisionSkins.Values)
             {
