d55c1d7eef97c36630f0a2fd854054da64a68470
krytra@gmail.com <krytra@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-06-03 23:25:51 +0000

    Poprawione Dropowanie itemów, particle emitter przeniesiony z Firearm do StorableObject

--- Zaraza/trunk/Plague/Plague/LowLevelGameFlow/GameObjects/Firearm.cs
+++ Zaraza/trunk/Plague/Plague/LowLevelGameFlow/GameObjects/Firearm.cs
@@ -54,20 +54,17 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
                          int descriptionWindowWidth,
                          int descriptionWindowHeight,
                          bool sideArm,
-                         AreaParticleEmitterComponent particle)
+                         ParticleEmitterComponent particle)
         {
             this.mesh = mesh;
-            this.body = body;
-
-            this.particle = particle;
+            this.body = body;            
 
             this.basicAttack = basicAttack;
             this.additionalAttack = additionalAttack;
-
-
+            
             SideArm = sideArm;
 
-            Init(icon, slotsIcon, description, descriptionWindowWidth, descriptionWindowHeight);
+            Init(icon, slotsIcon, description, descriptionWindowWidth, descriptionWindowHeight, particle);
         }
         /****************************************************************************/
 
@@ -83,13 +80,13 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
                 World *= Matrix.CreateRotationY(MathHelper.ToRadians(-90));
                 if (mesh != null) mesh.Enabled = true;
                 if (body != null) body.DisableBody();
-                if(particle!=null) particle.enabled = false;
+                if (particle != null) particle.enabled = false;
             }
             else
             {
                 if (getWorld != null) World = GetWorld();
-                if (body != null) body.EnableBody();
-                if (mesh != null) mesh.Enabled = true;
+                if (body     != null) body.EnableBody();
+                if (mesh     != null) mesh.Enabled = true;
                 if (particle != null) particle.enabled = true;
             }
         }
@@ -139,33 +136,6 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
             
             data.SideArm          = SideArm;
 
-
-            data.duration = particle.particleSystem.settings.DurationInSeconds;
-            data.durationRandomnes = particle.particleSystem.settings.DurationRandomness;
-            data.emitterVelocitySensitivity = particle.particleSystem.settings.EmitterVelocitySensitivity;
-            data.endVelocity = particle.particleSystem.settings.EndVelocity;
-            data.gravity = particle.particleSystem.settings.Gravity;
-            data.maxColor = particle.particleSystem.settings.MaxColor;
-            data.maxEndSize = particle.particleSystem.settings.MaxEndSize;
-            data.maxHorizontalVelocity = particle.particleSystem.settings.MaxHorizontalVelocity;
-            data.maxParticles = particle.particleSystem.settings.MaxParticles;
-            data.maxRotateSpeed = particle.particleSystem.settings.MaxRotateSpeed;
-            data.maxStartSize = particle.particleSystem.settings.MaxStartSize;
-            data.maxVerticalVelocity = particle.particleSystem.settings.MaxVerticalVelocity;
-            data.minColor = particle.particleSystem.settings.MinColor;
-            data.minEndSize = particle.particleSystem.settings.MinEndSize;
-            data.minHorizontalVelocity = particle.particleSystem.settings.MinHorizontalVelocity;
-            data.minRotateSpeed = particle.particleSystem.settings.MinRotateSpeed;
-            data.minStartSize = particle.particleSystem.settings.MinStartSize;
-            data.minVerticalVelocity = particle.particleSystem.settings.MinVerticalVelocity;
-            data.particleTexture = particle.particleSystem.settings.TextureName;
-            data.particlesPerSecond = particle.particlesPerSecond;
-            data.particleTranslation = particle.particleTranslation;
-            data.SpawnAreaLength = particle.areaLength;
-            data.SpawnAreaWidth = particle.areaWidth;
-            data.ParticlesEnabled = particle.enabled;
-
-
             return data;
         }
         /****************************************************************************/
@@ -188,10 +158,7 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
                 body = null;
             }
 
-
-            particle.ReleaseMe();
-
-
+            base.ReleaseComponents();
         }
         /****************************************************************************/
 
@@ -205,6 +172,32 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
             mesh.Enabled = false;
 
             base.OnStoring();
+        }
+        /****************************************************************************/
+        
+
+        /****************************************************************************/
+        /// On Dropping
+        /****************************************************************************/
+        public override void OnDropping()
+        {
+            body.EnableBody();
+            mesh.Enabled = true;
+
+            base.OnDropping();
+        }
+        /****************************************************************************/
+
+
+        /****************************************************************************/
+        /// On Picking
+        /****************************************************************************/
+        public override void OnPicking()
+        {
+            mesh.Enabled = true;
+            body.DisableBody();
+
+            base.OnPicking();
         }
         /****************************************************************************/
 
@@ -294,96 +287,6 @@ namespace PlagueEngine.LowLevelGameFlow.GameObjects
 
         [CategoryAttribute("Weapon Genre")]
         public bool SideArm { get; set; }
-
-
-
-
-
-        [CategoryAttribute("ParticlesSettings")]
-        public bool ParticlesEnabled { get; set; }
-
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("How long these particles will last.")]
-        public double duration { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("If greater than zero, some particles will last a shorter time than others")]
-        public float durationRandomnes { get; set; }
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Translation against gameObject position.")]
-        public Vector3 particleTranslation { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Alpha blending settings.1 - Additive, 2 - AlphaBlend, 3 - NonPremultiplied, 4 - Opaque. ")]
-        public int blendState { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Maximum number of particles that can be displayed at one time.")]
-        public int maxParticles { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Name of the texture used by this particle system.")]
-        public String particleTexture { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Frequency at which particles will be created.")]
-        public float particlesPerSecond { get; set; }
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling the particle color and alpha. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public Color minColor { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling the particle color and alpha. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public Color maxColor { get; set; }
-
-
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how big the particles are when first created. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float minStartSize { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how big the particles are when first created. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float maxStartSize { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how big particles become at the end of their life. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float minEndSize { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how big particles become at the end of their life. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float maxEndSize { get; set; }
-
-
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Controls how the particle velocity will change over their lifetime. If set to 1, particles will keep going at the same speed as when they were created.If set to 0, particles will come to a complete stop right before they die. Values greater than 1 make the particles speed up over time.")]
-        public float endVelocity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how much Y axis velocity to give each particle. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float minVerticalVelocity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how much X and Z axis velocity to give each particle. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float maxVerticalVelocity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how much X and Z axis velocity to give each particle. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float minHorizontalVelocity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how much X and Z axis velocity to give each particle. Values for individual particles are randomly chosen from somewhere between min and max.")]
-        public float maxHorizontalVelocity { get; set; }
-
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Direction and strength of the gravity effect. Note that this can point in any direction, not just down! The fire effect points it upward to make the flames rise, and the smoke plume points it sideways to simulate wind.")]
-        public Vector3 gravity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Controls how much particles are influenced by the velocity of the object which created them. You can see this in action with the explosion effect, where the flames continue to move in the same direction as the source projectile. The projectile trail particles, on the other hand, set this value very low so they are less affected by the velocity of the projectile.")]
-        public float emitterVelocitySensitivity { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how fast the particles rotate. Values for individual particles are randomly chosen from somewhere between min and max. If both these values are set to 0, the particle system will automatically switch to an alternative shader technique that does not support rotation, and thus requires significantly less GPU power. This means if you don't need the rotation effect, you may get a performance boost from leaving these values at 0.")]
-        public float maxRotateSpeed { get; set; }
-        [CategoryAttribute("ParticlesSettings"),
-        DescriptionAttribute("Range of values controlling how fast the particles rotate. Values for individual particles are randomly chosen from somewhere between min and max. If both these values are set to 0, the particle system will automatically switch to an alternative shader technique that does not support rotation, and thus requires significantly less GPU power. This means if you don't need the rotation effect, you may get a performance boost from leaving these values at 0.")]
-        public float minRotateSpeed { get; set; }
-
-
-        [CategoryAttribute("ParticlesSettings")]
-        public float SpawnAreaLength { get; set; }
-        [CategoryAttribute("ParticlesSettings")]
-        public float SpawnAreaWidth { get; set; }
 
     }
     /********************************************************************************/
