2be0f10c317551db6568d81dd2f1c7b34fb453f0
lukaszm89@gmail.com <lukaszm89@gmail.com@4632268c-c068-b875-c29a-90d66e3aa78c>
2011-06-26 21:59:52 +0000

    Wrzutka brakującego ExpireClock

---
+++ Zaraza/branches/NuclexAdd/Plague/Plague/TimeControlSystem/ExpireClock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlagueEngine.TimeControlSystem
+{
+    class ExpireClock
+    {
+        private TimeSpan _expireTime;
+        private DateTime _expireDate;
+        public ExpireClock(TimeSpan time)
+        {
+            _expireTime = time;
+            _expireDate = DateTime.Now;
+        }
+        public bool isExpired(){
+            if(_expireDate<=DateTime.Now){
+                _expireDate = DateTime.Now + _expireTime;
+                return true;
+            }
+            return false;
+        }
+        static public  ExpireClock FromSeconds(double time)
+        {
+            return new ExpireClock(TimeSpan.FromSeconds(time));
+        } 
+        static public ExpireClock FromMinutes(double time)
+        {
+            return new ExpireClock(TimeSpan.FromMinutes(time));
+        } 
+    }
+}
